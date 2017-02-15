784b703fcd82b6a02ecaccced0b4fe71b9dbcb22
Gala <Gala>
2017-02-15 05:17:01 +0200

    add comments

--- js/script1.js
+++ js/script1.js
@@ -1,7 +1,7 @@
 function pow(x, n) {
   var res = x;
 
-  if (n > 0) {
+  if (n > 0) { // положительная степень
 
     for (i = 1; i < n; i++) {
       res *= x;
@@ -9,7 +9,7 @@ function pow(x, n) {
 
     return console.log(res);
 
-  } else if (n < 0) {
+  } else if (n < 0) { // отрицательная степень
 
     n *= -1;
     for (i = 1; i < n; i++) {
@@ -18,7 +18,7 @@ function pow(x, n) {
 
     return console.log(1 / res);
 
-  } else if (n == 0) {
+  } else if (n == 0) { // нулевая степень
     return 1;
   }
 }
@@ -26,7 +26,7 @@ function pow(x, n) {
 var x = prompt('Введите целое число');
 var n = prompt('Введите степень, в которую требуется возвести число');
 
-if ( ( (x%1) !== 0) || ( (n%1) !== 0) ) { // проверяет целое ли введенное число
+if ( ( (x % 1) !== 0) || ( (n % 1) !== 0) ) { // проверяет целое ли введенное число
 
   alert('Ошибка! Введите число заново');
 
--- js/script2.js
+++ js/script2.js
@@ -4,22 +4,27 @@ for (i = 0; i < 5; i++) {
 
   var name = prompt('Введите '+ i +' имя');
 
-  if (name === 'null' || name === "") {
+  if (name === 'null' || name === "") { // проверка правильности введения элементов массива
       alert("Имя не введено");
       break;
+
   } else {
-      usersNames.push(name);
+      usersNames.push(name); // наполнение массива
   }
 }
 
-console.log(usersNames);
+if (usersNames.length < 5) {
+  alert("Введите все заново");
+}
+
+console.log(usersNames); // вывод в консоль заполненного массива
 
 var searchName = prompt("Введите имя пользователя");
 var search;
 
   for (var i = 0; i < usersNames.length; i++) {
 
-    if (searchName === usersNames[i]) {
+    if (searchName === usersNames[i]) { // сравнение имен в массиве с введенным
     search = true;
     alert(searchName + ', вы успешно вошли');
     }
